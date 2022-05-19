94db02e3dea7df79aeb6f3d87ed75d69d9c3c5d9
Kongo <takykongoabdoul@gmail.com>
2022-05-19 16:04:42 +0000

    Calculatrice_V1

--- Exercice_Javascript/Calculatrice_Js/script.js
+++ Exercice_Javascript/Calculatrice_Js/script.js
@@ -8,9 +8,54 @@ function calculer()
     let b = eval(a) 
     document.getElementById("ecran").value = b 
 }
-function effacer()
+function delet()
 {
     document.getElementById("ecran").value = " ";
 }
-
-
+function fermer() {
+    window.close();
+}
+function log() {
+    let resultat = Math.log(ecran.value);
+    ecran.value = resultat;
+}
+function tan() {
+    let resultat = Math.tan(ecran.value);
+    ecran.value = resultat;
+}
+function cos() {
+    let resultat = Math.cos(ecran.value);
+    ecran.value = resultat;
+}
+function sin() {
+    let resultat = Math.sin(ecran.value);
+    ecran.value = resultat;
+}
+function ln() {
+    let resultat = Math.LN10(ecran.value);
+    ecran.value = resultat;
+}
+function expo() {
+    let resultat = Math.exp(ecran.value);
+    ecran.value = resultat;
+}
+function abs(){
+    let resultat = Math.abs(ecran.value);
+    ecran.value=resultat;
+}
+function effacer() {
+    var ecran = document.getElementById("ecran").value;
+    document.getElementById("ecran").value = ecran.substring(0, ecran.length - 1);
+}
+function puissance2() {
+    ecran = document.getElementById("ecran");
+    ecran.value = ecran.value * ecran.value;
+}
+function racine(){
+    let resultat = Math.sqrt(ecran.value);
+    ecran.value=resultat;
+}
+function py(){
+    let resultat = Math.PI(ecran.value) += " ";
+    ecran.value=resultat;
+}
